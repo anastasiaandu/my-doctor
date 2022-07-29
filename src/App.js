9253ee3e678cfb1886b5f44d19c511ec736f1578
Pranav Prashar <prasherlucky@gmail.com>
2022-07-29 14:32:11 -0400

    Input compponent added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import PageHeader from "./components/PageHeader/PageHeader";
 import PageFooter from "./components/PageFooter/PageFooter";
+import InputComponent from "./components/InputComponent/InputComponent";
 
 function App() {
   return (
     <div>
       <PageHeader />
+      <InputComponent />
       <PageFooter />
     </div>
   );
