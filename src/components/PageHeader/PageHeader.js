be4604bd113936b83db42141fd114d359f6be2f7
Audun <audunyoung@gmail.com>
2022-07-29 11:33:40 -0600

    Change page header logo and color.

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,11 +1,12 @@
 import './PageHeader.scss';
+import pageLogo from '../../assets/logo/healthcareLogo/Wordmark.png';
 
 
 const PageHeader = () => {
     return (
         <div className="component">
             <div className="header">
-                <h1 className="header__logo">Logo</h1>
+                <img className="header__logo" src={pageLogo} alt='my doctor logo' />
                 <div className="header__pages">
                     <a href="/" className="header__pages-link">Home</a>
                     <a href="/" className="header__pages-link">About Us</a>
